e0bfe58b6811b4374a06879967dab4d43cf76719
EstrHuP <estrhup@gmail.com>
2026-03-12 13:40:25 +0100

    Create project for iOS 26

---
+++ GhibliFilmsUI/GhibliFilmsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliFilmsUIApp.swift
+//  GhibliFilmsUI
+//
+//  Created by EstherHuP on 12/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliFilmsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliFilmsUITests/GhibliFilmsUITests.swift
@@ -0,0 +1,16 @@
+//
+//  GhibliFilmsUITests.swift
+//  GhibliFilmsUITests
+//
+//  Created by EstherHuP on 12/3/26.
+//
+
+import Testing
+
+struct GhibliFilmsUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
